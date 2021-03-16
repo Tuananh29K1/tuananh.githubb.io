85940bfac61d0bccdaf65696225000b77904e99b
Tuananh29K1 <Tuananh29K1@users.noreply.github.com>
2021-03-16 23:25:28 +0700

    updatw'

--- buoi9-JS/bvnjs.js
+++ buoi9-JS/bvnjs.js
@@ -1,77 +1,29 @@
-
-/** Bài 1 */
-
-
-function xacDinhMua(thang){
-    if(thang >= 1 && thang <= 3){
-        console.log('Mùa Xuân');
-    }else if(thang >= 4 && thang <= 6){
-        console.log('Mùa Hè');
-    }else if(thang >= 7 && thang <= 9){
-        console.log('Mùa Thu');
-    }else {
-        console.log('Mùa Đông');
-    } 
-}
-
-var d = new Date();
-var m = d.getMonth();
-console.log(d);
-xacDinhMua(m);
-
-
-/** bài 2 */
- function soNgayTrongThang (y, m){
-   let day = new Date(y, m, 0).getDate(); 
-   console.log(`Tháng ${m} có ${day} ngày`);
-} 
-
-
-
-
-/** bài 3 
- * Tính tổng chữ số có trong 1 số nguyên dương
-*/
- function tinhTongChuSo1 (number){
-     let tachSo = number % 10;
-     console.log(tachSo);
- }
-       
-  
-
-
-
-
-
-function tinhTongChuSo (n){
-    let tachSo = n%10;
-    let tong = 0;
-    do 
-}
-/** bài 4 */
-function layChuoi(string){
-    if(string.length > 10){
-       let a = string.slice(0, 10);
-       console.log(`${a} +  ... `);
-    }else{
-        console.log('không hợp lệ');
+/**Cho một số nguyên dương N,
+ * viết hàm in ra màn hình những số hoàn hảo nhỏ hơn N
+ * (số hoàn hảo là số bằng
+ * tổng các ước số của nó mà không kể chính nó). */
+function showSo(N) {
+  for (let i = 0; i < N; i++) {
+    var sum = 0;
+    for (let j = 0; j <= i; j++) {
+      if (i % j == 0) {
+        sum += j;
+      }
+      if (sum == i) {
+        console.log("Đây là số hoàn hảo", i);
+      } else {
+        console.log(i);
+      }
     }
+  }
 }
 
-/** bài  5 */  
-function soNguyenTo(n){
-    let refult;
-    if( n < 2 ){
-        refult =false;
-        console.log(refult);
-    }else if(n == 2){
-        refult = true;
-        console.log(refult);
-    }else if(n % 2 == 0){
-        refult =false;
-        console.log(refult);
-    } 
+/** Tìm số nhỏ nhất và lớn nhất trong mảng số nguyên */
+function soLonNhat(array){
+    var maxInNumbers = Math.max.apply(Math, array); 
+    var minInNumbers = Math.min.apply(Math, array);
+    console.log(`đây là số nhỏ nhất ${minInNumbers} và số lớn nhất là ${maxInNumbers}`);
 }
 
-  
 
+/**Tìm số lớn thứ 2 trong mảng số nguyên */
